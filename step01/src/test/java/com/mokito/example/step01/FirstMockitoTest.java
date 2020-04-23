687a323d51d4d277de09594b4183a8276dba3307
Emad Hanna <Emad_Hanna@cable.comcast.com>
2020-04-23 19:13:53 -0400

    Commit the code

---
+++ step01/src/test/java/com/mokito/example/step01/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.mokito.example.step01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue("Assert True is True", true);
+	}
+
+}
